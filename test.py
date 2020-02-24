ca098cd209f90a157d82c4c9a8dea1f2b304e08c
FelixIzm <for_cv@mail.ru>
2020-02-24 16:10:03 +0300

    'first'

---
+++ test.py
@@ -0,0 +1,47 @@
+import requests, base64, json, re
+img_url = 'https://obd-memorial.ru/html/getimageinfo?id=70782617&_=1582546278185'
+
+info_url = 'https://obd-memorial.ru/html/info.htm?id=70782617'
+#####################################
+def make_str_cookie(cookies):
+    str_cook = ''
+    for key, value in cookies.items():
+        str_cook += '{0}={1};'.format(key,value)
+    return str_cook
+
+res1 = requests.get(info_url,allow_redirects = True)
+print(res1.status_code)
+print(res1.cookies)
+print('*****************')
+
+cookies = {}
+cookies['3fbe47cd30daea60fc16041479413da2']=res1.cookies['3fbe47cd30daea60fc16041479413da2']
+cookies['JSESSIONID']=res1.cookies['JSESSIONID']
+headers={}
+headers['Cookie'] = make_str_cookie(cookies)
+res2 = requests.get(info_url,headers=headers,cookies=cookies,allow_redirects = True)
+print(res2.status_code)
+print(res2.cookies)
+print('*****************')
+
+
+res3 = requests.get(img_url,headers=headers,cookies=cookies,allow_redirects = True)
+print(res3.status_code)
+#print(res3.text)
+data = json.loads(res3.text)
+#print(str(data[0]))
+result = re.findall(r'src=\"(\S+)\"', str(data[0]))
+print (result)
+result = re.findall(r'id=\"(\d+)\"', str(data[0]))
+print(len(result))
+
+'''
+cookies = {}
+cookies['3fbe47cd30daea60fc16041479413da2']=res2.cookies['3fbe47cd30daea60fc16041479413da2']
+cookies['JSESSIONID']='8C691120DCB05507637ABE7BCC8657F1'
+headers={}
+headers['Cookie'] = make_str_cookie(cookies)
+
+res3 = requests.get(url, cookies,allow_redirects = True)
+print(res3.status_code)
+'''
