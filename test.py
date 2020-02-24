acb75458949f6115b987dd3139c813fdeb23b800
FelixIzm <for_cv@mail.ru>
2020-02-24 20:42:54 +0300

    'img'

--- test.py
+++ test.py
@@ -11,6 +11,12 @@ def make_str_cookie(cookies):
     for key, value in cookies.items():
         str_cook += '{0}={1};'.format(key,value)
     return str_cook
+#####################################
+def getStringHash(id):
+    h = hashlib.md5(str(id).encode()+b'db76xdlrtxcxcghn7yusxjcdxsbtq1hnicnaspohh5tzbtgqjixzc5nmhybeh')
+    p = h.hexdigest()
+    return str(p)
+#####################################
 
 res1 = requests.get(info_url,allow_redirects = True)
 print(res1.status_code)
@@ -32,16 +38,24 @@ res3 = requests.get(img_url,headers=headers,cookies=cookies,allow_redirects = Tr
 print(res3.status_code)
 #print(res3.text)
 data = json.loads(res3.text)
-print(str(data[0].id))
+print(data[0]['id'])
 src = re.findall(r'src=\"(\S+)\"', str(data[0]))
-print (src)
-ids = re.findall(r'id=\"(\d+)\"', str(data[0]))
-print(len(ids))
+if(src):
+    print (src)
+    ids = re.findall(r'id=\"(\d+)\"', str(data[0]))
+    print(len(ids))
+    req="https://cdn.obd-memorial.ru/html/images3?id="+str(data[0]['id'])+"&id1="+(getStringHash(data[0]['id']))+"&path="+src[0]
+    print(req)
+'''
+Function Get-StringHash([String] $String,$HashName = "MD5") {
+    $StringBuilder = New-Object System.Text.StringBuilder
+    [System.Security.Cryptography.HashAlgorithm]::Create($HashName).ComputeHash([System.Text.Encoding]::UTF8.GetBytes($String+'db76xdlrtxcxcghn7yusxjcdxsbtq1hnicnaspohh5tzbtgqjixzc5nmhybeh'))|%{
+        [Void]$StringBuilder.Append($_.ToString("x2"))
+    }
+    $StringBuilder.ToString()
+}
 
-h = hashlib.md5(b"password")
-p = h.hexdigest()
-print(p)
-#req="http://obd-memorial.ru/memorial/fullimage?id="+$id_page+"&id1="+( Get-StringHash $id_page)+"&path="+$path_pic
+'''
 
 exit(1)
 
