d52882a0f656553683dede4ee305b22aa4931c0b
FelixIzm <for_cv@mail.ru>
2020-02-25 06:05:54 +0200

    ass

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-import requests, base64, json, re
+import requests, base64, json, re, os
 import hashlib
 import lxml.html as html
 #from lxml.html import parse, fromstring
@@ -33,56 +33,51 @@ def get_info(id,heareds,cookies):
 
 
 res1 = requests.get(info_url,allow_redirects = True)
-print(res1.status_code)
-print(res1.cookies)
-print('*****************')
+if(res1.status_code==307):
+    print(res1.status_code)
+    print('*****************')
 
-cookies = {}
-cookies['3fbe47cd30daea60fc16041479413da2']=res1.cookies['3fbe47cd30daea60fc16041479413da2']
-cookies['JSESSIONID']=res1.cookies['JSESSIONID']
-headers={}
-headers['Cookie'] = make_str_cookie(cookies)
-res2 = requests.get(info_url,headers=headers,cookies=cookies,allow_redirects = True)
-print(res2.status_code)
-print(res2.cookies)
-print('*****************')
+    cookies = {}
+    cookies['3fbe47cd30daea60fc16041479413da2']=res1.cookies['3fbe47cd30daea60fc16041479413da2']
+    cookies['JSESSIONID']=res1.cookies['JSESSIONID']
+    headers={}
+    headers['Cookie'] = make_str_cookie(cookies)
+    res2 = requests.get(info_url,headers=headers,cookies=cookies,allow_redirects = True)
+    if(res2.status_code==200):
+        print(res2.status_code)
+        print('*****************')
 
-get_info(345,headers,cookies)
+        #get_info(345,headers,cookies)
+        res3 = requests.get(img_url,headers=headers,cookies=cookies,allow_redirects = True)
+        if(res3.status_code==200):
+            print(res3.status_code)
+            data = json.loads(res3.text)
+            print(len(data))
+            page = data[0]
+            src = re.findall(r'src=\"(\S+)\"', str(page))
+            img_url="https://cdn.obd-memorial.ru/html/images3?id="+str(page['id'])+"&id1="+(getStringHash(page['id']))+"&path="+src[0]
+            print(page['id'])
+            print(img_url)
+            r = requests.get(img_url,headers=headers,cookies=cookies, allow_redirects=True)
+            header = r.headers
+            content_length = header.get('content-length', None)
+            print(content_length)
+            content_type = header.get('content-type')
+            '''
+            for page in data:
+                #print(page['id'])
+                src = re.findall(r'src=\"(\S+)\"', str(page))
+                if(src):
+                    print (src)
+                    ids = re.findall(r'id=\"(\d+)\"', str(page))
+                    print('%s : %s' % (page['id'],len(ids)))
+                    img_url="https://cdn.obd-memorial.ru/html/images3?id="+str(page['id'])+"&id1="+(getStringHash(page['id']))+"&path="+src[0]
+                    location = os.path.abspath("./scan/"+str(page['id'])+'.jpg')
 
-exit(1)
+                    r = requests.get(img_url,headers=headers,cookies=cookies, allow_redirects=True)
+                    open(location, 'wb').write(r.content)
 
-res3 = requests.get(img_url,headers=headers,cookies=cookies,allow_redirects = True)
-print(res3.status_code)
-#print(res3.text)
-data = json.loads(res3.text)
-print(data[0]['id'])
-src = re.findall(r'src=\"(\S+)\"', str(data[0]))
-if(src):
-    print (src)
-    ids = re.findall(r'id=\"(\d+)\"', str(data[0]))
-    print(len(ids))
-    req="https://cdn.obd-memorial.ru/html/images3?id="+str(data[0]['id'])+"&id1="+(getStringHash(data[0]['id']))+"&path="+src[0]
-    print(req)
-'''
-Function Get-StringHash([String] $String,$HashName = "MD5") {
-    $StringBuilder = New-Object System.Text.StringBuilder
-    [System.Security.Cryptography.HashAlgorithm]::Create($HashName).ComputeHash([System.Text.Encoding]::UTF8.GetBytes($String+'db76xdlrtxcxcghn7yusxjcdxsbtq1hnicnaspohh5tzbtgqjixzc5nmhybeh'))|%{
-        [Void]$StringBuilder.Append($_.ToString("x2"))
-    }
-    $StringBuilder.ToString()
-}
-
-'''
-
-exit(1)
-
-'''
-cookies = {}
-cookies['3fbe47cd30daea60fc16041479413da2']=res2.cookies['3fbe47cd30daea60fc16041479413da2']
-cookies['JSESSIONID']='8C691120DCB05507637ABE7BCC8657F1'
-headers={}
-headers['Cookie'] = make_str_cookie(cookies)
-
-res3 = requests.get(url, cookies,allow_redirects = True)
-print(res3.status_code)
-'''
+                    print(img_url)
+            '''    
+#https://cdn.obd-memorial.ru/html/images3?id=70782618&id1=e9b750f3f4568d8f049698151ca6772c&path=Z/011/058-A-0071744-0001/00000001.jpg
+#https://cdn.obd-memorial.ru/html/images3?id=70782618&id1=e9b750f3f4568d8f049698151ca6772c&path=Z/011/058-A-0071744-0001/00000001.jpg
